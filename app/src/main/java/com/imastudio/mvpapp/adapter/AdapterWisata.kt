ab404218a7af46c000c0e4b026839e7630a78aae
isoneday <iswandi.saputra11@gmail.com>
2019-11-13 16:14:17 +0700

    update sore2

--- app/src/main/java/com/imastudio/mvpapp/adapter/AdapterWisata.kt
+++ app/src/main/java/com/imastudio/mvpapp/adapter/AdapterWisata.kt
@@ -6,9 +6,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.imastudio.mvpapp.R
+import com.imastudio.mvpapp.helper.Helper.Companion.DATAWISATA
 import com.imastudio.mvpapp.model.modelwisata.DataItem
+import com.imastudio.mvpapp.view.activity.DetailWisataActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.tampilanwisata.view.*
+import org.jetbrains.anko.startActivity
 
 class AdapterWisata(
     var activity: FragmentActivity?,
@@ -26,18 +29,21 @@ class AdapterWisata(
 
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bindItem(item: DataItem) {
+        fun bindItem(item: DataItem, act: FragmentActivity) {
             Picasso.get().load(item.gambar).placeholder(R.drawable.empty)
                 .error(R.drawable.empty).into(itemView.itemImg)
             itemView.itemJudul.text = item.namaTempat
             itemView.itemDesk.text = item.deskripsi
-
+            itemView.setOnClickListener {
+                act.startActivity<DetailWisataActivity>(DATAWISATA to item)
+            }
 
         }
     }
+
     //untuk menghitung total data yg akan di load
     override fun getItemCount(): Int = dataWisata!!.size
 
-    override fun onBindViewHolder(holder: AdapterWisata.MyViewHolder, position: Int)=
-        holder.bindItem(dataWisata?.get(position)!!)
+    override fun onBindViewHolder(holder: AdapterWisata.MyViewHolder, position: Int) =
+        holder.bindItem(dataWisata?.get(position)!!,activity!!)
 }
---
+++ app/src/main/java/com/imastudio/mvpapp/view/activity/DetailWisataActivity.kt
@@ -0,0 +1,13 @@
+package com.imastudio.mvpapp.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.imastudio.mvpapp.R
+
+class DetailWisataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_wisata)
+    }
+}
